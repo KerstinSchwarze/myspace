bceee407640d1d09f75e7f3a57b89300780a86e7
KerstinSchwarze <kerstin.schwarze@capgemini.com>
2018-08-31 09:13:24 +0000

    test

--- src/main/java/io/openshift/booster/service/GreetingEndpoint.java
+++ src/main/java/io/openshift/booster/service/GreetingEndpoint.java
@@ -29,7 +29,8 @@ public class GreetingEndpoint {
     @GET
     @Produces("application/json")
     public Greeting greeting(@QueryParam("name") @DefaultValue("World") String name) {
-        final String message = String.format(Greeting.FORMAT, name);
+        final String message = String.format(Greeting.FORMAT, name)+ "hey fun";
+        
         return new Greeting(message);
     }
 }
